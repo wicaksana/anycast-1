71fe1d2460a5010ed30bf8f657b081b75a903d13
Roya Ensafi <rensafi@cs.princeton.edu>
2016-02-06 16:30:23 -0800

    gmap based on ip lat long asn

---
+++ code/probe-extract-lat-long.py
@@ -0,0 +1,16 @@
+import json
+import csv
+from pprint import pprint
+data_file = "../data/RIPE-Atlas-measurement-2048556.json"
+prob_geo = "../data/meta-latest.out"
+
+with open(prob_geo) as data_file:
+    data = json.load(data_file)
+
+geolocation_probes = [(item['address_v4'],item['latitude'],item['longitude'],item['asn_v4']) for item in data['objects'] if item['address_v4']]
+
+
+with open('../data/probes-lat-long-asn.csv', 'wb') as testfile:
+    csv_writer = csv.writer(testfile)
+    for y in geolocation_probes:
+        csv_writer.writerow(y)
